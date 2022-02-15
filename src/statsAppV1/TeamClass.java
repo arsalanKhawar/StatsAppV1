8de6dc3cec46a3696736c90afe22f8325059d894
arsalanKhawar <ak78@njit.edu>
2022-02-15 13:59:01 -0500

    added instance variables to teamclass for team points, team name, and a list of players. Created a constructor to construct team name

--- src/statsAppV1/TeamClass.java
+++ src/statsAppV1/TeamClass.java
@@ -1,4 +1,16 @@
 package statsAppV1;
 
+import java.util.ArrayList;
+
 public class TeamClass {
+
+    private int teamPoints = 0;
+    private String teamName;
+    private ArrayList<String> players = new ArrayList<>();
+
+    public TeamClass(String teamName){
+        this.teamName = teamName;
+    }
+
+
 }
