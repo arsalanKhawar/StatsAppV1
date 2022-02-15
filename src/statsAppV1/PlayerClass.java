68b893359d096d352d030db0f5f81398d050b0f2
arsalanKhawar <ak78@njit.edu>
2022-02-15 13:20:12 -0500

    added getter methods for the player stats variables

--- src/statsAppV1/PlayerClass.java
+++ src/statsAppV1/PlayerClass.java
@@ -2,11 +2,37 @@ package statsAppV1;
 
 public class PlayerClass {
 
-private int points, assists,twoPoints,threePoints,blocks, steals = 0;
-private String playerName, playerTeam;
+    private int points, assists, twoPoints, threePoints, blocks, steals = 0;
+    private String playerName, playerTeam;
 
-public PlayerClass(String playerName, String playerTeam){
-    this.playerName = playerName;
-    this.playerTeam = playerTeam;
-}
+    public PlayerClass(String playerName, String playerTeam) {
+        this.playerName = playerName;
+        this.playerTeam = playerTeam;
+    }
+
+    //methods to get stats
+    public int getPoints(){
+        return points;
+    }
+
+    public int getAssists(){
+        return assists;
+    }
+
+    public int getTwoPoints(){
+        return twoPoints;
+    }
+
+    public int getThreePoints(){
+        return threePoints;
+    }
+
+    public int getBlocks(){
+        return blocks;
+    }
+
+    public int getSteals(){
+        return steals;
+    }
+
 }
