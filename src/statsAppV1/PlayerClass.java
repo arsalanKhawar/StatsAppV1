bae1ab0eddec9089969de6223ed2fc3153a7845f
arsalanKhawar <ak78@njit.edu>
2022-02-15 13:10:21 -0500

    Added instance variables, as well as a constructor for the player's name and team

--- src/statsAppV1/PlayerClass.java
+++ src/statsAppV1/PlayerClass.java
@@ -1,4 +1,12 @@
 package statsAppV1;
 
 public class PlayerClass {
+
+private int points, assists,twoPoints,threePoints,blocks, steals = 0;
+private String playerName, playerTeam;
+
+public PlayerClass(String playerName, String playerTeam){
+    this.playerName = playerName;
+    this.playerTeam = playerTeam;
+}
 }
